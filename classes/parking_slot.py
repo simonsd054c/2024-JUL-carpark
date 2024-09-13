4d99226c53dcb9a35ddb3e2defa568bf732dfadf
Simon Dahal <simon.dahal@coderacademy.edu.au>
2024-09-14 09:51:38 +1000

    Initial setup for classes

---
+++ classes/parking_slot.py
@@ -0,0 +1,10 @@
+class ParkingSlot:
+    def __init__(self, id):
+        self.id = id
+        self.car = None
+    
+    def __str__(self):
+        if self.car:
+            return f"Parking slot with id {self.id} and has the following car: {self.car}"
+        else:
+            return f"Parking slot with id {self.id}"
